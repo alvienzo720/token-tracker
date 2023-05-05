85c25fca85cbbc13c945039eef5c9d96ca63a099
alvienzo720 <hudson1Dellmac>
2023-05-05 16:54:32 +0300

    Modify Balance Route

--- src/pages/api/balance.ts
+++ src/pages/api/balance.ts
@@ -5,11 +5,11 @@ import { getWeb3 } from '@/utils/web3';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     const web3 = getWeb3()
-    const { address }:any = req.query
+    const { address }: any = req.query
 
     try {
         const balanceWei = await web3.eth.getBalance(address)
-        const balanceEth = web3.utils.fromWei(balanceWei, 'ether')
+        const balanceEth = parseFloat(web3.utils.fromWei(balanceWei, 'ether')).toFixed(4)
         res.status(200).json({ balance: balanceEth })
     } catch (error) {
         res.status(400).json({ error: 'Invalid Ethereum, Address' })
